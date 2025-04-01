402db3cfb49e2fe7837076eba8413ac63282397a
千月 <155789279+2316663736@users.noreply.github.com>
2025-04-01 22:06:51 +0800

    finish of lab05

--- cs61a/test.py
+++ cs61a/test.py
@@ -1,50 +1,18 @@
-from operator import index
-
-
-def accuracy(typed, source):
-    """Return the accuracy (percentage of words typed correctly) of TYPED
-    compared to the corresponding words in SOURCE.
-
-    Arguments:
-        typed: a string that may contain typos
-        source: a model string without errors
-
-    >>> accuracy('Cute Dog!', 'Cute Dog.')
-    50.0
-    >>> accuracy('A Cute Dog!', 'Cute Dog.')
-    0.0
-    >>> accuracy('cute Dog.', 'Cute Dog.')
-    50.0
-    >>> accuracy('Cute Dog. I say!', 'Cute Dog.')
-    50.0
-    >>> accuracy('Cute', 'Cute Dog.')
-    100.0
-    >>> accuracy('', 'Cute Dog.')
-    0.0
-    >>> accuracy('', '')
-    100.0
-    """
-    typed_words = split(typed)
-    source_words = split(source)
-    # BEGIN PROBLEM 3
-    "*** YOUR CODE HERE ***"
-    num_same = 0
-    index_typed=0
-    index_source=0
-    for word_type in typed_words :
-        index_source=0
-        for word_source in source_words :
-            if word_type == word_source and index_source == index_typed :
-                num_same += 1
-            index_source += 1
-        index_typed += 1
-    return num_same / len(typed_words)*100
-    # END PROBLEM 3
-def split(s):
-    """Return a list of words contained in s, which are sequences of characters
-    separated by whitespace (spaces, tabs, etc.).
-
-    >>> split("It's a lovely day, don't you think?")
-    ["It's", 'a', 'lovely', 'day,', "don't", 'you', 'think?']
-    """
-    return s.split()
+class test:
+    def __init__(self):
+        self.a = 1
+    def pr(self):
+        print(self.v)
+    v=0
+test.v=3
+m=test()
+n=test()
+print(m.v)
+print(n.v)
+m.pr()
+n.pr()
+m.v=2
+print(m.v)
+print(n.v)
+m.pr()
+n.pr()

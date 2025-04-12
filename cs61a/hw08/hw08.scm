3b964d448918d6a6f44b356f03eba4510ee72c35
千月 <155789279+2316663736@users.noreply.github.com>
2025-04-12 19:38:37 +0800

    finish hw08 question 1

--- cs61a/hw08/hw08.scm
+++ cs61a/hw08/hw08.scm
@@ -1,4 +1,23 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (define (biger-than num lis)
+    (
+      if
+      (null? lis)
+      #t
+      (
+        if
+        (<= num (car lis))
+        (biger-than (car lis) (cdr lis))
+        #f
+      )
+    )
+  )
+  (if
+    (null? s )
+    #t
+    (biger-than (car s) (cdr s))
+  )
+)
 
 (define (my-filter pred s) 'YOUR-CODE-HERE)
 
